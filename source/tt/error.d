ffd1205529f4979dc6eee8312d0e147406fa38ab
Temtaime <temtaime@gmail.com>
2016-09-20 18:14:31 +0300

    Restore throwErrorImpl.

--- source/tt/error.d
+++ source/tt/error.d
@@ -4,26 +4,18 @@ import
 		std.string,
 		std.format;
 
-bool throwError(string file = __FILE__, uint line = __LINE__, A...)(string fmt, auto ref A args)
+
+auto throwError(string F = __FILE__, uint L = __LINE__, A...)(auto ref A args)
+{
+	return throwErrorImpl(F, L, args);
+}
+
+bool throwErrorImpl(A...)(string f, uint l, string fmt, auto ref A args)
 {
 	static if(args.length)
 	{
 		fmt = format(fmt, args);
 	}
 
-	throw new Exception(fmt, file, line);
+	throw new Exception(fmt, f, l);
 }
-
-/*bool throwErrorImpl(A...)(string f, uint l, A args)
-{
-	static if(args.length == 1)
-	{
-		throw new Exception(format(`[%s, %u]: %s`, f, l, args));
-	}
-	else
-	{
-		return throwErrorImpl(f, l, format(args));
-	}
-}*/
-
-//bool throwError(string f = __FILE__, uint l = __LINE__, A...)(A args) { return throwErrorImpl(f, l, args); }
